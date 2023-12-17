21f98d291e7edf3e8225bcb7cc09c0f7762d93ed
Suresh B <sureshui1990@gmail.com>
2023-12-17 20:00:56 +0530

    content edit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>React app deployment</h3>
+        <h3>Ramkumar KP karuppu entru jacks kalis</h3>
       </header>
     </div>
   );
