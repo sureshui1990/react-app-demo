c17031e3d3c43078753f79d18c9da67bb4916e11
Suresh B <sureshui1990@gmail.com>
2023-12-17 20:08:27 +0530

    content edit -  chromastone

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Ramkumar KP karuppu entru jacks kalis</h3>
+        <h3>Chroma stone</h3>
       </header>
     </div>
   );
